3c8f86e2e074d7dfcf255d39be63b67315e31563
Karel Maxa <karel.maxa@orchitech.cz>
2024-03-22 15:01:11 +0100

    Refactor index page.

---
+++ site/.vitepress/theme/components/index/Features.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  columns: { type: Number, required: true }
+});
+</script>
+
+<template>
+  <div class="wren-features">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.wren-features {
+  display: grid;
+  grid-template-columns: repeat(v-bind(columns), 1fr);
+  gap: 16px;
+  margin-top: 42px;
+}
+
+@media (max-width: 960px) {
+  .wren-features {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+</style>
+
---
+++ site/.vitepress/theme/components/index/Project.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="project-wrapper">
+    <h1>
+      <slot name="title" />
+    </h1>
+    <p>
+      <slot name="description" />
+    </p>
+    <div class="project-features">
+      <slot name="features" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.project-wrapper {
+  padding-top: 80px;
+  /* border-top: 1px solid var(--vp-c-divider); */
+}
+.project-features {
+  /* display: flex;
+  column-gap: 28px;
+  margin-top: 28px; */
+}
+.project-features :slotted(ul) {
+  display: grid;
+  /* flex-wrap: wrap; */
+  list-style: none;
+  padding-left: 0;
+  /* column-gap: 28px; */
+  /* row-gap: 14px; */
+  margin-top: 42px;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  justify-items: stretch;
+  align-items: stretch;
+}
+</style>
